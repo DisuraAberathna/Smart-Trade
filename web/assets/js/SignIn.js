9ae7c8d230a76bfb9ca3b74ca5bf3e6be6818d7d
Disura Chathwara <disura2005@gmail.com>
2024-09-04 17:58:40 +0530

    SignIn.js file updated

---
+++ web/assets/js/SignIn.js
@@ -0,0 +1,31 @@
+const signIn = async() => {
+    const reqObject = {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+    };
+
+    const resp = await fetch(
+            "SignIn",
+            {
+                method: "POST",
+                body: JSON.stringify(reqObject),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+
+    if (resp.ok) {
+        const respObject = await resp.json();
+        if (respObject.success) {
+            window.location = "home.html";
+        } else {
+            if (respObject.content === "Not Verified") {
+                window.location = "verify-account.html"
+            } else {
+                document.getElementById("message").innerHTML = respObject.content;
+            }
+        }
+    } else {
+        document.getElementById("message").innerHTML = "Please try again later!";
+    }
+};
