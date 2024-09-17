6fcd309213fc8f746a68b4546142aca7b710b774
Disura Chathwara <disura2005@gmail.com>
2024-09-18 00:49:12 +0530

    sign-in js programmed

---
+++ web/assets/js/sign-in.js
@@ -0,0 +1,37 @@
+async function signIn(){
+    
+    const user_dto = { 
+      email:document.getElementById("email").value,  
+      password:document.getElementById("password").value 
+    };
+
+    const response = await fetch(
+            "SignIn",
+    {
+        method:"POST",
+        body:JSON.stringify(user_dto),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    });
+    
+    if(response.ok){
+        
+        const json = await response.json();
+        
+        if(json.success){
+            window.location = "index.html";
+        }else{
+            
+            if(json.content==="Unverified"){
+                window.location = "verify-account.html";
+            }else{
+                document.getElementById("message").innerHTML = json.content;
+            }
+            
+        }
+        
+    }else{
+        document.getElementById("message").innerHTML = "Please try again later.";
+    }
+}
